93fbd1f87cf26595db6830de1add795d0302eb1d
c00lkiddpostshaxxs <bean.haipham43@gmail.com>
2026-08-10 15:54:31 +0700

    Update commit.js

--- api/commit.js
+++ api/commit.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send('Method not allowed');
   }
