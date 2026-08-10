9443f83ddcb2f99dea01f267c257cbb1be6bd24f
c00lkiddpostshaxxs <bean.haipham43@gmail.com>
2026-08-10 15:53:59 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,14 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
   const CALLBACK_URL = process.env.CALLBACK_URL || 'https://c00lkiddpostshaxxs.vercel.app/api/callback';
   
